70fc592d47fea77d167b739a5dfc39d14e024c32
yingbo-wu <yingbo.wu@outlook.com>
2023-03-11 16:27:06 +0800

    Update pull-k8s-image.sh

--- pull-k8s-image.sh
+++ pull-k8s-image.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 k8s_img=$1
-mirror_img=$(echo ${k8s_img}|
-        sed 's/quay\.io/wuyingbo\/quay/g;s/ghcr\.io/wuyingbo\/ghcr/g;s/registry\.k8s\.io/wuyingbo\/google-containers/g;s/k8s\.gcr\.io/wuyingbo\/google-containers/g;s/gcr\.io/wuyingbo/g;s/\//\./g;s/ /\n/g;s/wuyingbo\./wuyingbo\//g' |
-        uniq)
+mirror_img=$(echo ${k8s_img} | sed 's/quay\.io/wuyingbo\/quay/g;s/ghcr\.io/wuyingbo\/ghcr/g;s/registry\.k8s\.io/wuyingbo\/google-containers/g;s/k8s\.gcr\.io/wuyingbo\/google-containers/g;s/gcr\.io/wuyingbo/g;s/\//\./g;s/ /\n/g;s/wuyingbo\./wuyingbo\//g' | uniq)
 
 if [ -x "$(command -v docker)" ]; then
   sudo docker pull ${mirror_img}
